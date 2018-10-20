429ab4f67b1f36acaa7391022ed5df6614ec42b6
Mikhail Guskov <meguskov@gmail.com>
2018-10-20 17:51:52 +0300

    использование process.uptime, promisify readFile  и другие мелкие улучшения

--- server/index.js
+++ server/index.js
@@ -2,35 +2,31 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const {promisify} = require('util')
+const readFileAsync = promisify(fs.readFile);
 const dateDiff = require('./utils/dateDiff');
 
-var startTime = null;
 const port = 8000;
 const types = ['info', 'critical'];
 const _path = './events.json';
 
 app.get('/status', (request, response) => {
-    response.send(dateDiff(startTime, new Date()));
+    response.send(dateDiff(process.uptime()));
 });
 
 app.get('/api/events', (request, response, next) => {
-    fs.readFile(path.join(__dirname, _path), 'utf-8', (err, data) => {
-        try {
-            if (err) {
-                throw err;
-            }
-
-            var result = JSON.parse(data).events;
+    readFileAsync(path.join(__dirname, _path), {encoding: 'utf8'})
+        .then((data) => {
+            let result = JSON.parse(data).events;
 
             //Если в запросе переданы параметры
-            if (typeof request.query['type'] !== 'undefined') {
+            if (request.query.type && request.query.type !== '') {
                 const queryTypes = request.query.type.split(':');
 
                 //проверка соответсвуют ли переданные параметры доступным
                 for (let index = 0; index < queryTypes.length; index++) {
                     if (!types.includes(queryTypes[index])) {
-                        response.status(400).send('incorrect type');
-                        return;
+                        return response.status(400).send('incorrect type');
                     }
                 }
 
@@ -38,14 +34,14 @@ app.get('/api/events', (request, response, next) => {
             }
 
             response.json({ events: result });
-        } catch (e) {
-            next(e);
-        }
-    });
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
-app.get('*', (request, response) => {
-    response.status(404).send('<h1>Page not found</h1>');
+app.use((request, response, next) => {
+    response.type('text/html').status(404).send('<h1>Page not found</h1>');
 });
 
 app.use((err, request, response, next) => {
@@ -57,6 +53,5 @@ app.listen(port, err => {
         return console.log('[Error]: ', err);
     }
 
-    startTime = new Date();
     console.log('server is listening on', port);
 });
