ac400c99db386e941600754e918968bde977cd80
Mikhail Guskov <meguskov@gmail.com>
2018-10-20 17:51:52 +0300

    использование process.uptime, promisify readFile  и другие мелкие улучшения

--- server/utils/dateDiff.js
+++ server/utils/dateDiff.js
@@ -1,24 +1,11 @@
-module.exports = function dateDiff(date1, date2) {
-    var hours, minutes, seconds;
-    hours = date2.getUTCHours() - date1.getUTCHours();
-    minutes = date2.getUTCMinutes() - date1.getUTCMinutes();
-    seconds = date2.getUTCSeconds() - date1.getUTCSeconds();
-
-    if (seconds < 0) {
-        seconds += 60;
-        minutes--;
-    }
-    if (minutes < 0) {
-        minutes += 60;
-        hours--;
-    }
-    if (hours < 0) {
-        hours += 24;
-    }
+module.exports = function dateDiff(duration) {
+    let seconds = parseInt(duration % 60),
+        minutes = parseInt((duration / 60) % 60),
+        hours = parseInt((duration / 360) % 24);
 
     if (seconds < 10) seconds = '0' + seconds;
     if (minutes < 10) minutes = '0' + minutes;
     if (hours < 10) hours = '0' + hours;
 
-    return `${hours}:${minutes}:${seconds}`;
-}
+    return hours + ':' + minutes + ':' + seconds;
+};
