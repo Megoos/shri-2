b05acabef31de4c01444ae1f07802f9198187bb5
Mikhail Guskov <meguskov@gmail.com>
2018-10-29 03:08:37 +0300

    собрал сборку через gulp

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,1 @@
-"use strict";
-// открытие меню в мобильной версии
-var hamburger = document.querySelectorAll('.hamburger-menu-link');
-var closeEl = document.querySelectorAll('.close');
-var menu = document.querySelectorAll('.device-menu');
-closeEl[0].onclick = function (event) {
-    event.preventDefault();
-    menu[0].style.display = 'none';
-};
-hamburger[0].onclick = function (event) {
-    event.preventDefault();
-    menu[0].style.display = 'block';
-};
+"use strict";var hamburger=document.querySelectorAll(".hamburger-menu-link"),closeEl=document.querySelectorAll(".close"),menu=document.querySelectorAll(".device-menu");closeEl[0].onclick=function(e){e.preventDefault(),menu[0].style.display="none"},hamburger[0].onclick=function(e){e.preventDefault(),menu[0].style.display="block"};
